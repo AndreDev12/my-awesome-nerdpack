024ecc97d1ce2ec573de2885542d697ed8ab9221
André Torres <andretorresjava@gmail.com>
2023-09-02 23:59:52 -0500

    First commit

---
+++ nerdlets/home/index.js
@@ -0,0 +1,30 @@
+// https://docs.newrelic.com/docs/new-relic-programmable-platform-introduction
+import React, { useState, useEffect } from 'react';
+import { BillboardChart } from 'nr1';
+
+function HomeNerdlet() {
+  const [count, setCount] = useState(0);
+  useEffect(() => {
+    setTimeout(() => {
+      setCount(() => count + 1);
+    }, 1000);
+  });
+
+  const clickCount = {
+    metadata: {
+      id: '1',
+      name: 'Count',
+      viz: 'main',
+    },
+    data: [{ y: count }],
+  };
+
+  return (
+    <div>
+      <BillboardChart data={[clickCount]} />
+      <button onClick={() => setCount(count + 1)}>Increment count</button>
+    </div>
+  );
+}
+
+export default HomeNerdlet;
